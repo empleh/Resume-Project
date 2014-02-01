24b0877c4738f2b3c3ff5c1b1dc6702303460e8e
empleh <christian.peters@gmail.com>
2014-02-01 10:42:09 -0600

    Initial Upload

---
+++ resume.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){init();});
+
+function init() {
+    PopulateTechnicalSkills($("#technicalSkills"));   
+}
+
+function PopulateTechnicalSkills(tbl) {
+    var tableData = new Array();
+    $.getJSON("technicalSkills.json", function(data) {
+        var technicalSkills = data["Technical Skills"];
+        SortSkills(technicalSkills);
+        technicalSkills.sort(function(a, b){});
+                             
+        $.each(technicalSkills, function(index, item){
+            tableData.push("<tr>");
+            tableData.push("<td>" + item["Skill"] + "</td>");
+            tableData.push("<td>" + item["Years"] + "</td>");
+            tableData.push("<td>" + item["Level"] + "</td>");
+            tableData.push("</tr>");           
+        });
+        tbl.append(tableData.join(""));
+});
+}
+
+function SortSkills(skills) {
+    var levelValues = {"Expert":0,"Advanced":1,"Intermediate":2,"Beginner":3};
+    skills.sort(function(a, b){
+        if (a.Level === b.Level){
+            if (a.Years === b.Years){
+                return SkillCompare(a.Skill, b.Skill);
+            }else{
+                return YearsCompare(a.Years, b.Years);
+            }
+        }else{
+            return LevelCompare(a.Level, b.Level, levelValues);
+        }
+    });
+}
+
+function SkillCompare(a, b){
+    return a > b ? 1 : -1;
+}
+
+function YearsCompare(a, b){
+    console.log(a);
+    return a > b ? -1 : 1;
+}
+
+function LevelCompare(a, b, comparer){
+    return comparer[a] > comparer[b] ? 1 : -1;
+}
